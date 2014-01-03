dd8ec2603b2e7fa797f5b4f55be6eb1d8db5e4cf
Bruno Costa <bruno.costa@hardquarters.com>
2014-01-03 18:48:48 +0000

    Cool Team base code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coolteam::Application.config.secret_token = 'bcfcf9a49c9ae0a3b4ae8fd428182e1d903ff6c58f94e3917aff2729056d31377253a6f552ccbfd2bd715d5857fb53b92d3c4f12c86f8347806ed4654dfe6ea8'
