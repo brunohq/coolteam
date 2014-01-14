f0a7fe2f1250754f4085925922360b6a5c340fcf
Bruno Costa <bruno.costa@hardquarters.com>
2014-01-14 18:57:43 +0000

    mail copy changes

--- app/mailers/collaborator_mailer.rb
+++ app/mailers/collaborator_mailer.rb
@@ -3,6 +3,6 @@ class CollaboratorMailer < ActionMailer::Base
 
   def daily_email(collaborator)
     @collaborator = collaborator
-    mail(:to => collaborator.email, :subject => "Olá, como estás hoje?")
+    mail(:to => collaborator.email, :subject => "Olá, como te sentes hoje?")
   end
 end
