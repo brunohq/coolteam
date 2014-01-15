6cac11e285229df06264276a1089c004dd60e902
Bruno Costa <bruno.costa@hardquarters.com>
2014-01-15 17:41:52 +0000

    prevent users from creating an empty mood

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -4,17 +4,21 @@ class MoodsController < ApplicationController
     @collaborator = Collaborator.find(params[:collaborator_id])
     @mood = Mood.where(:date=>Date.today).where(:collaborator_id=>params[:collaborator_id]).first
 
-	if @mood
-	  @mood.rating = params[:mood][:rating]
-	  @mood.comment = params[:mood][:comment]
-	  @mood.save
-	else
-      @mood = @collaborator.moods.new(params[:mood])
-      @mood.date = Date.today
-	  @mood.save
-	end
+    unless params[:mood][:rating].blank?
+		if @mood
+		  @mood.rating = params[:mood][:rating]
+		  @mood.comment = params[:mood][:comment]
+		  @mood.save
+		else
+	      @mood = @collaborator.moods.new(params[:mood])
+	      @mood.date = Date.today
+		  @mood.save
+		end
 
-    redirect_to report_collaborators_path
+	    redirect_to report_collaborators_path
+	else		
+		redirect_to login_path(@collaborator.unique_token), :flash => { :danger => "Tem de selecionar um estado antes de clicar no botao actualizar." }
+	end
   end
 
 end
